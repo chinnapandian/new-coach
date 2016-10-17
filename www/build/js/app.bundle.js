42481fab7bf0f0658c83ddf36e33e1d1c42b3e53
Sam <samuelpierce@outlook.com>
2016-10-17 07:37:41 -0400

    style changes

--- www/build/js/app.bundle.js
+++ www/build/js/app.bundle.js
@@ -2032,31 +2032,31 @@ var SelectTeamPage = (function () {
         this.teams = [
             {
                 name: 'Boston Celtics',
-                img: '../../../../team-logos/celtics.png',
+                img: 'team-logos/celtics.png',
                 coach: 'Brad Stevens'
             }, {
                 name: 'New York Knicks',
-                img: '../../../../team-logos/knicks.png',
+                img: 'team-logos/knicks.png',
                 coach: 'Jeff Hornacek'
             }, {
                 name: 'Brooklyn Nets',
-                img: '../../../../team-logos/nets.jpg',
+                img: 'team-logos/nets.jpg',
                 coach: 'Kenny Atkinson'
             }, {
                 name: 'Miami Heat',
-                img: '../../../../team-logos/heat.png',
+                img: 'team-logos/heat.png',
                 coach: 'Micky Arison'
             }, {
                 name: 'Golden State Warriors',
-                img: '../../../../team-logos/warriors.png',
+                img: 'team-logos/warriors.png',
                 coach: 'Steve Kerr'
             }, {
                 name: 'San Antonio Spurs',
-                img: '../../../../team-logos/spurs.jpeg',
+                img: 'team-logos/spurs.jpeg',
                 coach: 'Gregg Popovich'
             }, {
                 name: 'Seattle Super Sonics',
-                img: '../../../../team-logos/new-medal.svg',
+                img: 'team-logos/new-medal.svg',
                 coach: 'Kevin Tirone'
             }
         ];
